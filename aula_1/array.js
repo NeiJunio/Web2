54f7592335d7c97759ef86b524e9da966b546a68
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-03-26 20:34:04 -0300

    Introdução ao conceito de array
    estrutura basica de array

--- aula_1/array.js
+++ aula_1/array.js
@@ -1,4 +1,17 @@
-let aluno1 = 'Nei Junio';
+
+let alunos = ['Nei Junio', 'Gabriel', 'Matheus', 'Weslley'];
+console.log('Lista de alunos: ');
+console.log('');
+
+for (let n = 0; n < alunos.length; n++) {
+    console.log('- ' + alunos[n]);
+}
+
+console.log('');
+console.log('Fim do programa');
+
+
+/*let aluno1 = 'Nei Junio';
 let aluno2 = 'Gabriel';
 let aluno3 = 'Matheus';
 let aluno4 = 'Weslley';
@@ -11,4 +24,4 @@ console.log('- ' + aluno3);
 console.log('- ' + aluno4);
 console.log('');
 
-console.log('Fim do programa');
+console.log('Fim do programa');*/
